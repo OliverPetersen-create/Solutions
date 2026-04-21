e88d1b235dffc3b2bce2a9bff49340fa138508e5
OliverPetersen-create <60748@edu.campusvejle.dk>
2026-04-21 11:35:59 +0200

    .

---
+++ 0160_square_numbers_exercise.py
@@ -0,0 +1,29 @@
+"""
+Opgave "square numbers":
+
+Som altid skal du læse hele opgavebeskrivelsen omhyggeligt, før du begynder at løse opgaven.
+
+Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
+
+-------
+
+Skriv kode i funktionen, som printer alle kwadrattal (1, 4, 9, ...), som er mindre end limit.
+
+-------
+
+Hvis du går i stå, så spørg google, de andre elever, en AI eller læreren.
+
+Når dit program er færdigt, skal du skubbe det til dit github-repository.
+Fortsæt derefter med den næste fil.
+"""
+
+
+def print_squarenumbers(limit):
+    for ints in range(limit):
+        kvadrattal = ints ** 2
+        if kvadrattal >= limit:
+            break
+        print(kvadrattal)
+
+
+print_squarenumbers(700)
