38b45c6a7a046885f66702e77b2c5c61649257e7
OliverPetersen-create <60748@edu.campusvejle.dk>
2026-04-21 12:01:24 +0200

    .

---
+++ 0200_spell_consonants_exercise.py
@@ -0,0 +1,39 @@
+"""
+Opgave "spell_consonants":
+
+Som altid skal du læse hele opgavebeskrivelsen omhyggeligt, før du begynder at løse opgaven.
+
+Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
+
+-------
+
+Forandre koden i funktionen spell_consonants() sådan at den gør hvad der står i dens dokumentation.
+
+-------
+
+Hvis du går i stå, så spørg google, de andre elever, en AI eller læreren.
+
+Når dit program er færdigt, skal du skubbe det til dit github-repository.
+Fortsæt derefter med den næste fil.
+"""
+
+ignore_letters = ['a', 'e', 'i', 'o', 'u', 'y']
+
+def spell_consonants(text, letter_limit):
+    """
+    Spells/prints the first letter_limit letters of text.
+    Prints only consonants and spaces (a, e, i, o, u, y do not get printed). """
+    index = 0
+    while index < letter_limit:
+        if index >= len(text):
+            break
+        if text[index] in ignore_letters:
+            index += 1
+            continue
+        print(text[index], end="")
+        index += 1
+    print("")
+
+
+spell_consonants("Hello world", 9)  # should print "Hll wr"
+spell_consonants("I love Python", 10)  # should print "I lv Pt"
