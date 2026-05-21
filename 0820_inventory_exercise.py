4678f2e5802c6032216247ceb8806d9442d7d2d9
OliverPetersen-create <60748@edu.campusvejle.dk>
2026-05-21 11:57:44 +0200

    .

---
+++ 0820_inventory_exercise.py
@@ -0,0 +1,56 @@
+"""Opgave "The inventory sequence"
+
+Som altid skal du læse hele opgavebeskrivelsen omhyggeligt, før du begynder at løse opgaven.
+
+--------
+
+Denne øvelse er en valgfri udfordring for de fremragende programmører blandt jer.
+Du behøver absolut ikke at løse denne øvelse for at fortsætte med succes.
+
+Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
+
+Del 1:
+    Se de første 3 minutter af denne video:
+    https://www.youtube.com/watch?v=rBU9E-ZOZAI
+
+Del 2:
+    Skriv en funktion inventory(), som producerer de tal, der er vist i videoen.
+    Funktionen accepterer en parameter, der definerer, hvor mange talrækker der skal produceres.
+    Funktionen udskriver tallene i hver række.
+
+    Du vil sandsynligvis ønske at definere en funktion count_number(), som tæller, hvor ofte
+    et bestemt antal optræder i den aktuelle talrække.
+
+Del 3:
+    I hovedprogrammet kalder du inventory() med fx 6 som argument.
+
+--------
+
+Hvis du går i stå, så spørg google, de andre elever, en AI eller læreren.
+
+Når dit program er færdigt, skal du skubbe det til dit github-repository.
+"""
+
+
+def inventory(lines):
+	current_inventory = []
+	for i in range(lines):
+		current = 0
+		while True:
+			amount_of = count_numbers(current_inventory, current)
+			current_inventory.append(amount_of)
+			print(f"{amount_of}", end=" ")
+			if amount_of == 0:
+				break
+			current += 1
+		print()
+
+def count_numbers(inv, number):
+	count = 0
+	for numbers in inv:
+		if numbers == number:
+			count += 1
+	return count
+
+
+inventory(6)
